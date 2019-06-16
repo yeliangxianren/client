474ba9963f24ac5b6112fb872ef34a54355d9c90
yeliangxianren <756803284@qq.com>
2019-06-16 08:49:03 +0800

    Update logining.
    Update logining.

--- the client/src/ClientModle/Client2Controller.java
+++ the client/src/ClientModle/Client2Controller.java
@@ -1,6 +1,8 @@
 package ClientModle;
 
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
 import com.google.gson.Gson;
 
@@ -10,8 +12,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import ClientModle.FundAccount;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+
 import java.lang.Object;
 public class Client2Controller implements Initializable {
 	@FXML 
@@ -28,22 +32,30 @@ public class Client2Controller implements Initializable {
 	@FXML 
 	public void LOGIN_M(ActionEvent event)
 	{
-	FundAccount Login = new FundAccount();
-	String Accounts = account.getText();
-	String Passwords = password.getText();
-	Login.setFundId(Integer.parseInt(Accounts));
-    Login.setPassword(Passwords);
-	String json = new Gson().toJson(Login);
-//
-     CustomResp cr = new HttpCommon().doHttp("/client/login", "POST", json);
-     		new HttpCommon().doHttp("/stock/all", "GET", null);
-       	//if(cr.getResultJSON().r.equals("false"))
- System.out.println(cr.getResultJSON());
-
-	application.gotoclient();
-	
+		FundAccount Login = new FundAccount();
+		String Accounts = account.getText();
+		String Passwords = password.getText();
+		Login.setFundId(Integer.parseInt(Accounts));
+	    Login.setPassword(Passwords);
+		String json = new Gson().toJson(Login);
+		CustomResp cr = new HttpCommon().doHttp("/client/login", "POST", json);
+//     	new HttpCommon().doHttp("/stock/all", "GET", null);
+     
+		Gson gson = new Gson();
+		Map<String, Object> map = new HashMap<String, Object>();
+		map = gson.fromJson(cr.getResultJSON(), map.getClass());
+     
+		if((boolean)map.get("status"))
+			application.gotoclient();
+		else {
+			Alert alert = new Alert(AlertType.INFORMATION);
+			alert.setTitle("ERROR");
+			alert.setHeaderText("登录失败");
+			alert.setContentText((String)map.get("cause"));
+			alert.showAndWait();
+		}
 	}
 	
-	 @Override
-	    public void initialize(URL url, ResourceBundle rb){}
+	@Override
+	public void initialize(URL url, ResourceBundle rb){}
 }
